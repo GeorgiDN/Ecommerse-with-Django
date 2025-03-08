8251f7e1704f187d011c696a899b9a20884c2e6a
GeorgiDN <go6odn28@gmail.com>
2025-03-08 15:53:18 +0200

    feat(cart): add messages when the product is added, updated or deleted from cart

--- ecommerseApp/ecommerseApp/cart/views.py
+++ ecommerseApp/ecommerseApp/cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from ecommerseApp.cart.cart import Cart
 from ecommerseApp.store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 
 def cart_summary(request):
@@ -31,7 +32,7 @@ def cart_add(request):
 
         cart_quantity = cart.__len__()
 
-        # return JsonResponse({'Product Name': product.name})
+        messages.success(request, 'The product has been added to the cart.')
         return JsonResponse({'qty': cart_quantity})
 
     return JsonResponse({'error': 'Invalid request'}, status=400)
@@ -47,6 +48,7 @@ def cart_update(request):
         cart.update(product=product_id, quantity=product_qty)
 
         response = JsonResponse({'qty': product_qty})
+        messages.success(request, 'You cart has been updated.')
         return response
 
     return redirect('cart_summary')
@@ -61,6 +63,7 @@ def cart_delete(request):
         cart.delete(product=product_id)
 
         response = JsonResponse({'product': product_id})
+        messages.success(request, 'Item has been deleted from shopping cart.')
         return response
 
     return redirect('cart_summary')
