7dcb70249603f8cc144832d9a0fc26dd5b3651ae
GeorgiDN <go6odn28@gmail.com>
2025-03-11 12:32:32 +0200

    feat(payment): add billing-info page

--- ecommerseApp/ecommerseApp/payment/urls.py
+++ ecommerseApp/ecommerseApp/payment/urls.py
@@ -4,4 +4,5 @@ from ecommerseApp.payment import views as p_views
 urlpatterns = [
     path('payment_success', p_views.payment_success, name='payment_success'),
     path('checkout', p_views.checkout, name='checkout'),
+    path('billing_info', p_views.billing_info, name='billing_info'),
 ]
--- ecommerseApp/ecommerseApp/payment/views.py
+++ ecommerseApp/ecommerseApp/payment/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from ecommerseApp.cart.cart import Cart
 from ecommerseApp.payment.forms import ShippingForm
 from ecommerseApp.payment.models import ShippingAddress
+from django.contrib import messages
 
 
 def checkout(request):
@@ -26,6 +27,33 @@ def checkout(request):
         shipping_form = ShippingForm(request.POST or None)
         context['shipping_form'] = shipping_form
         return render(request, 'payment/checkout.html', context)
+
+
+def billing_info(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_products()
+        quantities = cart.get_quantity()
+        totals = cart.cart_total()
+
+        context = {
+            'cart_products': cart_products,
+            'quantities': quantities,
+            'totals': totals,
+        }
+
+        if request.user.is_authenticated:
+            context['shipping_info'] = request.POST
+            return render(request, 'payment/billing_info.html', context)
+        else:
+            ...
+
+        shipping_form = ShippingForm(request.POST or None)
+        context['shipping_form'] = shipping_form
+        return render(request, 'payment/billing_info.html', context)
+    else:
+        messages.success(request, 'Acces Denied')
+        return redirect('home')
 
 
 def payment_success(request):
