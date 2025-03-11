a1bd413e6662e1fef569121e8f53f49a9e132426
GeorgiDN <go6odn28@gmail.com>
2025-03-11 10:33:15 +0200

    feat(payment): add checkout page

--- ecommerseApp/ecommerseApp/payment/urls.py
+++ ecommerseApp/ecommerseApp/payment/urls.py
@@ -3,4 +3,5 @@ from ecommerseApp.payment import views as p_views
 
 urlpatterns = [
     path('payment_success', p_views.payment_success, name='payment_success'),
+    path('checkout', p_views.checkout, name='checkout'),
 ]
--- ecommerseApp/ecommerseApp/payment/views.py
+++ ecommerseApp/ecommerseApp/payment/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
 
+from ecommerseApp.cart.cart import Cart
+from ecommerseApp.payment.forms import ShippingForm
+from ecommerseApp.payment.models import ShippingAddress
+
+
+def checkout(request):
+    cart = Cart(request)
+    cart_products = cart.get_products()
+    quantities = cart.get_quantity()
+    totals = cart.cart_total()
+
+    context = {
+        'cart_products': cart_products,
+        'quantities': quantities,
+        'totals': totals,
+    }
+
+    if request.user.is_authenticated:
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        context['shipping_form'] = shipping_form
+        return render(request, 'payment/checkout.html', context)
+    else:
+        shipping_form = ShippingForm(request.POST or None)
+        context['shipping_form'] = shipping_form
+        return render(request, 'payment/checkout.html', context)
+
 
 def payment_success(request):
     return render(request, 'payment/payment_success.html')
