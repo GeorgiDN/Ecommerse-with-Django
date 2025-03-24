cb30117d925a84058cffb40aaef63c981baa5644
GeorgiDN <go6odn28@gmail.com>
2025-03-24 21:11:30 +0200

    test(raillay): change settings

--- ecommerseApp/ecommerseApp/settings.py
+++ ecommerseApp/ecommerseApp/settings.py
@@ -104,20 +104,45 @@ WSGI_APPLICATION = 'ecommerseApp.wsgi.application'
 # }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER_"),
-        'PASSWORD': os.getenv("DB_PASSWORD_"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv("DB_NAME"),
+#         'USER': os.getenv("DB_USER_"),
+#         'PASSWORD': os.getenv("DB_PASSWORD_"),
+#         'HOST': os.getenv("DB_HOST"),
+#         'PORT': os.getenv("DB_PORT"),
+#     }
+# }
 
+
+
+
+########################
 # DATABASES = {
 #     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 # }
+#####################
+
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT', '5432'),  # Default PostgreSQL port
+        }
+    }
+
 
 # DATABASE_URL = os.getenv('DATABASE_URL')
 #
