98fb154acfabbebdf9c8e24de33cd9e1ef9e44dd
GeorgiDN <go6odn28@gmail.com>
2025-04-20 18:37:14 +0300

    feat: enable to asign product in category in category page

--- ecommerseApp/ecommerseApp/store_admin/forms.py
+++ ecommerseApp/ecommerseApp/store_admin/forms.py
@@ -43,5 +43,38 @@ class CategoryCreateForm(CategoryBaseForm):
     pass
 
 
-class CategoryEditForm(CategoryBaseForm):
-    pass
+# class CategoryEditForm(CategoryBaseForm):
+#     pass
+
+
+class CategoryEditForm(forms.ModelForm):
+    products = forms.ModelMultipleChoiceField(
+        queryset=Product.objects.all(),
+        required=False,
+        widget=forms.SelectMultiple(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+        widgets = {
+            'description': forms.Textarea(attrs={
+                'rows': 5,
+                'cols': 60,
+                'class': 'form-control',
+                'placeholder': 'Enter category description here...'
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['products'].initial = self.instance.category_products.all()
+
+    def save(self, commit=True):
+        category = super().save(commit=False)
+        if commit:
+            category.save()
+        category.category_products.set(self.cleaned_data['products'])
+        self.save_m2m()
+        return category
