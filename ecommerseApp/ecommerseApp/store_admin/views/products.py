bda43659e744c359ac6a4ec3835633d84e0909d2
GeorgiDN <go6odn28@gmail.com>
2025-04-21 22:36:19 +0300

    feat: enable bulk add and remove products from categories

--- ecommerseApp/ecommerseApp/store_admin/views/products.py
+++ ecommerseApp/ecommerseApp/store_admin/views/products.py
@@ -2,12 +2,12 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.urls import reverse, reverse_lazy
-from ecommerseApp.store.models import Product
+from ecommerseApp.store.models import Product, Category
 from ecommerseApp.store_admin.forms import ProductEditForm, ProductCreateForm
 from ecommerseApp.store_admin.models_mixins import StaffRequiredMixin
 from django.views.generic import ListView, TemplateView, UpdateView, DeleteView, CreateView
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 
 
 class ProductCreateView(LoginRequiredMixin, StaffRequiredMixin, CreateView):
@@ -56,6 +56,7 @@ class AdminProductListView(LoginRequiredMixin, StaffRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['search_query'] = self.request.GET.get('q', '')
+        context['categories'] = Category.objects.all()
         return context
 
 
@@ -81,6 +82,30 @@ def bulk_edit_products(request):
             count = products.count()
             products.delete()
             messages.success(request, f"{count} product(s) deleted.")
+        elif action == 'is_available':
+            products.update(is_available=True)
+            messages.success(request, f"{products.count()} product(s) are available.")
+        elif action == 'not_available':
+            products.update(is_available=False)
+            messages.success(request, f"{products.count()} product(s) are not available.")
+        elif action == 'add_to_category':
+            category_id = request.POST.get('category_id')
+            if category_id:
+                category = get_object_or_404(Category, id=category_id)
+                updated_count = 0
+                for product in Product.objects.filter(id__in=selected_ids):
+                    product.categories.add(category)
+                    updated_count += 1
+                messages.success(request, f"{updated_count} products added to category '{category.name}'.")
+        elif action == 'remove_from_category':
+            category_id = request.POST.get('category_id')
+            if category_id:
+                category = get_object_or_404(Category, id=category_id)
+                updated_count = 0
+                for product in Product.objects.filter(id__in=selected_ids):
+                    product.categories.remove(category)
+                    updated_count += 1
+                messages.success(request, f"{updated_count} products removed from category '{category.name}'.")
         else:
             messages.warning(request, "No valid action selected.")
 
